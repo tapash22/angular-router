bd21c2b036435c8568ee2fb7cb825dd26b031847
tapash22 <tapasp263@gmail.com>
2025-05-05 23:15:46 +0600

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { importProvidersFrom } from '@angular/core';
+
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [ importProvidersFrom(FontAwesomeModule),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faHome } from '@fortawesome/free-solid-svg-icons';
+
 
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  iconBell = faBell;
+  iconHome = faHome
 
 }
