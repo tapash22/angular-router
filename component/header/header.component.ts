0efa65a64863c7db538a4af84233c49045efbfb4
tapash22 <tapasp263@gmail.com>
2025-05-06 22:33:49 +0600

    update

--- component/header/header.component.ts
+++ component/header/header.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  NavigationEnd, Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBell, faHome } from '@fortawesome/free-solid-svg-icons';
+import { filter } from 'rxjs';
 
 
 
@@ -11,8 +13,31 @@ import { faBell, faHome } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   iconBell = faBell;
   iconHome = faHome
+  lastSegment:string = ''
+  pathSegments:string[] = []
 
+  constructor(private router: Router) {}
+
+  // ngOnInit() {
+  //   this.router.events.pipe(
+  //     filter(event => event instanceof NavigationEnd)
+  //   ).subscribe((event: NavigationEnd) => {
+  //     const url = event.urlAfterRedirects; // safer than router.url
+  //     const segments = url.split('/').filter(Boolean); // removes empty strings
+  //     this.lastSegment = segments.length > 0 ? segments[segments.length - 1] : '';
+  //   });
+  // }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      const url = event.urlAfterRedirects;  // e.g., '/admin/user'
+      this.pathSegments = url.split('/').filter(Boolean);  // ['admin', 'user']
+      console.log(this.pathSegments);  // for debugging
+    });
+  }
 }
