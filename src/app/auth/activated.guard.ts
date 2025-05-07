9f9561638780987032e7af0bc9ec5375a919f593
tapash22 <tapasp263@gmail.com>
2025-05-07 23:48:22 +0600

    added route antivation and also added service for user dammy data and auth user

---
+++ src/app/auth/activated.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../service/auth/auth.service';
+
+export const activatedGuard: CanActivateFn = (route, state) => {
+
+  const auth = inject(AuthService)
+
+  if(auth.isAuthenticated()){
+    return true
+  }else{
+    auth.navigateByUrl('/login')
+    return false
+  }
+};
