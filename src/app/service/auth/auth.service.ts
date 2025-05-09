2a0d10a130813c4aa3c8ea267cf05df8d1895d15
tapash22 <tapasp263@gmail.com>
2025-05-10 01:50:59 +0600

    implement registration and login with user emil store

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from "@angular/core";
-import { User } from "../../interfaces/user";
+import { User,RegistrationPayload } from "../../interfaces/user";
 import { Router } from "@angular/router";
 import { MOCK_USERS } from "../../localStore/user-data";
 
@@ -7,13 +7,15 @@ import { MOCK_USERS } from "../../localStore/user-data";
   providedIn: "root",
 })
 export class AuthService {
-  private users: User[] = MOCK_USERS
+  private router = inject(Router) //import router
+
+  private users: User[] = MOCK_USERS //import user array
 
   private currentUser: User | null = null;
-  private router = inject(Router)
 
   constructor() {}
 
+  //login method
   login(email: string, password: string): boolean {
     const user = this.users.find(
       (u) => u.email === email && u.password === password
@@ -27,11 +29,37 @@ export class AuthService {
     return false;
   }
 
+//logout method
+
   logout():void{
     this.currentUser = null
     localStorage.removeItem('currentUser')
     this.navigateByUrl('/login')
   }
+
+//registration method
+  register(newUser: RegistrationPayload): boolean {
+  const exists = this.users.some((u) => u.email === newUser.email);
+
+  if (exists) {
+    return false; // Email already in use
+  }
+
+  const userToSave: User = {
+    id: Date.now(),  // unique ID
+    name: newUser.name,
+    email: newUser.email,
+    password: newUser.password,
+    role: 'user',  // default role
+  };
+
+  this.users.push(userToSave);
+  localStorage.setItem('user', JSON.stringify(this.users));
+  return true;
+}
+
+
+//current user information
 
   getCurrentUser():User | null{
 
@@ -45,14 +73,17 @@ export class AuthService {
     return this.currentUser
   }
 
+
   isAuthenticated(): boolean{
     return !!this.getCurrentUser()
   }
 
+  //role check
   hasRole(role: 'admin' |'manager'|'officer'|'user'): boolean{
     return this.getCurrentUser()?.role === role
   }
 
+  //router nagivation
   navigateByUrl(url:string):void{
     this.router.navigateByUrl(url,{replaceUrl:true})
   }
