22b5b56ef73fce04c66f10e28c26f02ec74aae3f
tapash22 <tapasp263@gmail.com>
2025-05-16 13:21:36 +0600

    update

--- src/app/auth/role.guard.ts
+++ src/app/auth/role.guard.ts
@@ -1,22 +1,18 @@
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../service/auth/auth.service';
-import { inject } from '@angular/core';
+import { CanActivateFn, Router } from "@angular/router";
+import { AuthService } from "../service/auth/auth.service";
+import { inject } from "@angular/core";
 
 export const roleGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService);
   const router = inject(Router);
 
   const currentUser = auth.getCurrentUser();
-  // const allowedRoles = route.data['allowedRoles'] as string[] | undefined;
 
-  // if (allowedRoles && currentUser?.role && allowedRoles.includes(currentUser.role)) {
-  //   return true;
-  // }
-    const allowedRole = route.routeConfig?.path; // 'admin', 'manager', etc
+  const allowedRole = route.routeConfig?.path; // 'admin', 'manager', etc
 
   if (currentUser?.role === allowedRole) {
     return true;
   }
 
-  return router.createUrlTree(['/auth/login']); // redirect if not allowed
+  return router.createUrlTree(["/auth/login"]); // redirect if not allowed
 };
