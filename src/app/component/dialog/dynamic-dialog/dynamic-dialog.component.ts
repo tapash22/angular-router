82383f78478ed8a56ebd27d4d7b301adc79b22bf
tapash22 <tapasp263@gmail.com>
2025-06-29 21:15:04 +0600

    upate

--- src/app/component/dialog/dynamic-dialog/dynamic-dialog.component.ts
+++ src/app/component/dialog/dynamic-dialog/dynamic-dialog.component.ts
@@ -1,37 +1,42 @@
-import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: "app-dynamic-dialog",
-  imports: [CommonModule],
-  templateUrl: "./dynamic-dialog.component.html",
-  styleUrl: "./dynamic-dialog.component.css",
+  selector: 'app-dynamic-dialog',
+  imports: [CommonModule, FontAwesomeModule],
+  templateUrl: './dynamic-dialog.component.html',
+  styleUrl: './dynamic-dialog.component.css',
 })
 export class DynamicDialogComponent {
   @Input() title!: string;
-  @Input() size: "tiny" | "sm" | "md" | "lg" | "xl" | "xxl" = "sm";
+  @Input() size: 'tiny' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' = 'sm';
   @Input() show = false;
   @Input() disableClose = false;
-  @Input() showFooter = true;
+  @Input() showFooter = false;
   @Input() submitBtnTitle!: string;
   @Output() onClose = new EventEmitter<void>();
 
+  // icon
+  iconClose = faTimes;
+
   get dialogWidth(): string {
     switch (this.size) {
-      case "tiny":
-        return "w-[20%]";
-      case "sm":
-        return "w-[30%]";
-      case "md":
-        return "w-[40%]";
-      case "lg":
-        return "w-[50%]";
-      case "xl":
-        return "w-[70%]";
-      case "xxl":
-        return "w-[80%]";
+      case 'tiny':
+        return 'w-[20%]';
+      case 'sm':
+        return 'w-[30%]';
+      case 'md':
+        return 'w-[40%]';
+      case 'lg':
+        return 'w-[50%]';
+      case 'xl':
+        return 'w-[70%]';
+      case 'xxl':
+        return 'w-[80%]';
       default:
-        return "w-[30%]";
+        return 'w-[30%]';
     }
   }
 
