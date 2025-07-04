89adb8bee5aba494a794ab97dde9c641af9d6d25
tapash22 <tapasp263@gmail.com>
2025-07-04 14:51:55 +0600

    update reuse code use

--- src/app/childs/dynamic-progress-bar/dynamic-progress-bar.component.ts
+++ src/app/childs/dynamic-progress-bar/dynamic-progress-bar.component.ts
@@ -12,4 +12,20 @@ export class DynamicProgressBarComponent {
   @Input() progressBarTitle!: string;
   @Input() score: number | null | undefined = 0;
   @Input() striped: boolean = false;
+  @Input() height: 'sm' | 'md' | 'lg' | 'xl' = 'sm';
+
+  get progressbarHeight(): string {
+    switch (this.height) {
+      case 'sm':
+        return 'h-2';
+      case 'md':
+        return 'h-3 ';
+      case 'lg':
+        return 'h-5 ';
+      case 'xl':
+        return 'h-7';
+      default:
+        return 'h-2';
+    }
+  }
 }
--- src/app/childs/project-work-resource/project-work-resource.component.ts
+++ src/app/childs/project-work-resource/project-work-resource.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ProjecctResources } from '../../interfaces/user';
+import { DynamicProgressBarComponent } from '../dynamic-progress-bar/dynamic-progress-bar.component';
 
 @Component({
   selector: 'app-project-work-resource',
-  imports: [CommonModule],
+  imports: [CommonModule,DynamicProgressBarComponent],
   templateUrl: './project-work-resource.component.html',
   styleUrl: './project-work-resource.component.css'
 })
