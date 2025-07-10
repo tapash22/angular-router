4c78ede0b33649eceabce15cc035aa65702e5595
tapash22 <tapasp263@gmail.com>
2025-07-11 04:46:17 +0600

    update with window resize and click event implement with rxjs into reset password page

---
+++ src/app/service/window-size/window-size.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  debounceTime,
+  fromEvent,
+  map,
+  Observable,
+  shareReplay,
+  startWith,
+} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WindowSizeService {
+  size$: Observable<{ width: number; height: number }> = fromEvent(
+    window,
+    'resize'
+  ).pipe(
+    debounceTime(500),
+    map(() => ({ width: window.innerWidth, height: window.innerHeight })),
+    startWith({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }),
+    shareReplay(1)
+  );
+}
--- src/app/views/reset-password/reset-password.component.ts
+++ src/app/views/reset-password/reset-password.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -7,18 +7,24 @@ import {
   ReactiveFormsModule,
   AbstractControl,
   ValidationErrors,
-} from "@angular/forms";
-import { ToasterService } from "../../service/toaster.service";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import { UserService } from "../../service/user.service";
-import { DynamicButtonComponent } from "../../childs/dynamic-button/dynamic-button.component";
-
+} from '@angular/forms';
+import { ToasterService } from '../../service/toaster.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { UserService } from '../../service/user.service';
+import { DynamicButtonComponent } from '../../childs/dynamic-button/dynamic-button.component';
+import { Observable, scan, Subject } from 'rxjs';
+import { WindowSizeService } from '../../service/window-size/window-size.service';
 @Component({
-  selector: "app-reset-password",
-  imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule,DynamicButtonComponent],
-  templateUrl: "./reset-password.component.html",
-  styleUrl: "./reset-password.component.css",
+  selector: 'app-reset-password',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
+    DynamicButtonComponent,
+  ],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.css',
 })
 export class ResetPasswordComponent {
   profileForm: FormGroup;
@@ -27,15 +33,30 @@ export class ResetPasswordComponent {
   iconEye = faEye;
   iconEyeClose = faEyeSlash;
 
+  // declear  Rx.jx
+
+  clickCount = 0;
+  private clickSubject = new Subject<void>();
+
+  // width = 0;
+  // height = 0;
+  size$: Observable<{ width: number; height: number }>;
+
+  smoothedWidth = 0;
+  targetWidth = 0;
+
+  // declear end  Rx.jx
+
   constructor(
     private fb: FormBuilder,
     private userService: UserService,
-    private toaster: ToasterService
+    private toaster: ToasterService,
+    private windowSizeService: WindowSizeService
   ) {
     this.profileForm = this.fb.group(
       {
         password: [
-          "",
+          '',
           [
             Validators.required,
             Validators.minLength(8),
@@ -44,37 +65,74 @@ export class ResetPasswordComponent {
             ),
           ],
         ],
-        confirmPassword: ["", Validators.required],
+        confirmPassword: ['', Validators.required],
       },
       { validators: this.passwordMatchValidator }
     );
+
+    // implement Rx.jx
+
+    this.size$ = windowSizeService.size$;
+    this.clickSubject
+      .pipe(
+        scan((count) => count + 1, 0) // accumulate count
+      )
+      .subscribe((count) => {
+        this.clickCount = count;
+        console.log(this.clickCount);
+      });
+
+    // implement Rx.jx end
+  }
+  ngOnInit() {
+    this.size$.subscribe((size) => {
+      this.targetWidth = size.width / 4;
+      this.animateWidth();
+    });
+  }
+
+  animateWidth() {
+    const step = () => {
+      const diff = this.targetWidth - this.smoothedWidth;
+      if (Math.abs(diff) < 1) {
+        this.smoothedWidth = this.targetWidth;
+        return;
+      }
+      this.smoothedWidth += diff * 0.2; // 10% easing
+      requestAnimationFrame(step);
+    };
+    requestAnimationFrame(step);
   }
 
   passwordMatchValidator(form: AbstractControl): ValidationErrors | null {
-    const password = form.get("password")?.value;
-    const confirmPassword = form.get("confirmPassword")?.value;
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
     return password === confirmPassword ? null : { mismatch: true };
   }
 
   onUpdate() {
     if (this.profileForm.valid) {
       //receive password form inpuut filed with check validation
-      const newPassword = this.profileForm.get("password")?.value;
+      const newPassword = this.profileForm.get('password')?.value;
 
       //call auth service for reset password
       const success = this.userService.resetPassword(newPassword);
 
       if (success) {
-        this.toaster.showToast("Reset password successfully!", "success");
+        this.toaster.showToast('Reset password successfully!', 'success');
         //reset form
         this.profileForm.reset();
         console.log(this.userService.getCurrentUser());
       } else {
-        this.toaster.showToast("Failed to Reset Password.", "error");
+        this.toaster.showToast('Failed to Reset Password.', 'error');
       }
     } else {
       // Show all validation errors immediately
       this.profileForm.markAllAsTouched();
     }
+  }
+
+  onClick() {
+    this.clickSubject.next(); // manually trigger the scan
   }
 }
